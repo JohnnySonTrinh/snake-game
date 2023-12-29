7275bccc2628db3e10d180fee5bcdf8215c3346b
Johnny Trinh <johnny.trinh@hotmail.se>
2023-12-29 07:02:10 +0100

    Replace ' with " for best practice

--- run.py
+++ run.py
@@ -5,33 +5,34 @@ from simple_term_menu import TerminalMenu
 
 
 SCOPE = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive.file',
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
-SHEET = GSPREAD_CLIENT.open('snake-game-scoreboard')
+SHEET = GSPREAD_CLIENT.open("snake-game-scoreboard")
 
 
 def clear():
     """
     Clears the terminal
     """
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def get_scoreboard():
     """
     Get the scoreboard from the spreadsheet
     """
-    scoreboard = SHEET.worksheet('scoreboard')
+    scoreboard = SHEET.worksheet("scoreboard")
 
     data = scoreboard.get_all_values()
 
     return data
+
 
 def start_game():
     clear()
